673ff17e78cb18352f889175060bf2655cc4854a
stittpaul2001 <stittpaul2001@gmail.com>
2025-04-18 14:40:34 -0600

    fixed the layout, added more styling, now moving onto more JS

---
+++ app.js
@@ -0,0 +1,77 @@
+
+
+const iceCreamFlavors = [
+  {
+    name: 'Cookie Dough',
+    price: 1.25,
+    quantity: 0,
+    type: 'scoop'
+  },
+
+  {
+    name: 'Vanilla',
+    price: 1,
+    quantity: 0,
+    type: 'scoop'
+  },
+  {
+    name: 'Strawberry',
+    price: 1.25,
+    quantity: 0,
+    type: 'scoop'
+  },
+]
+
+
+const vessels = [
+  {
+    Name: 'Waffle Cone',
+    price: 1.25,
+    quantity: 0,
+    type: 'vessel'
+  },
+  {
+    Name: 'Sugar Cone',
+    price: 1.25,
+    quantity: 0,
+    type: 'vessel'
+  },
+  {
+    Name: 'Cup',
+    price: 1.50,
+    quantity: 0,
+    type: 'vessel'
+  }
+]
+
+const toppings = [
+  {
+    name: 'Sprinkles',
+    price: 0.25,
+    quantity: 0,
+    type: 'topping'
+  },
+  {
+    name: 'Chocolate Chips',
+    price: 0.25,
+    quantity: 0,
+    type: 'topping'
+  },
+  {
+    name: 'Cookie Chunks',
+    price: 0.50,
+    quantity: 0,
+    type: 'topping'
+  }
+]
+
+
+function OrderIceCream() {
+
+
+}
+
+function orderVanilla() {
+  console.log('ordering vanilla, not samonilla')
+
+}
